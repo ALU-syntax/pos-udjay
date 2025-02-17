40f550c0e33f96e9e88316320cccdbe20c24f6b0
woodiechek <68280149+ALU-syntax@users.noreply.github.com>
2025-02-17 07:42:56 +0700

    create greeting birthday

---
+++ resources/views/email/birthday-greeting.blade.php
@@ -0,0 +1,269 @@
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+  <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title>
+    <!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]-->
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <!--[if gte mso 9]>
+<xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG></o:AllowPNG>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+    <!--[if !mso]><!-- -->
+    <link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
+    <!--<![endif]-->
+  </head>
+  <body class="body">
+    <div dir="ltr" class="es-wrapper-color">
+      <!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#f6f6f6"></v:fill>
+			</v:background>
+		<![endif]-->
+      <table width="100%" cellspacing="0" cellpadding="0" class="es-wrapper">
+        <tbody>
+          <tr>
+            <td valign="top" class="esd-email-paddings">
+              <table cellspacing="0" cellpadding="0" align="center" class="esd-header-popover es-header">
+                <tbody>
+                  <tr>
+                    <td align="center" class="esd-stripe">
+                      <table width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" class="es-header-body">
+                        <tbody>
+                          <tr>
+                            <td align="left" bgcolor="#f9eed8" class="es-p20t es-p20r es-p20l esd-structure" style="background-color:#f9eed8">
+                              <table cellspacing="0" cellpadding="0" width="100%">
+                                <tbody>
+                                  <tr>
+                                    <td width="560" valign="top" align="center" class="es-m-p0r esd-container-frame">
+                                      <table width="100%" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                          <tr>
+                                            <td align="center" class="esd-block-image" style="font-size:0px">
+                                              <a target="_blank" href="https://www.instagram.com/ud.djaya/">
+                                                <img src="{{ asset('img/Logo Red.png') }}" alt="Logo" width="300" title="Logo" style="display:block; margin-top: 20px;">
+                                              </a>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+                <br>
+                <br>
+              </table>
+
+
+              <table cellpadding="0" cellspacing="0" align="center" class="es-content">
+                <tbody>
+                  <tr>
+                    <td align="center" class="esd-stripe">
+                      <table bgcolor="#d03c3c" align="center" cellpadding="0" cellspacing="0" width="600" class="es-content-body" style="background-color:#d03c3c">
+                        <tbody>
+                          <tr></tr>
+                          <tr>
+                            <td align="left" bgcolor="#F9EED8" background="https://tlr.stripocdn.email/content/guids/CABINET_a6898ed919b3f403b400f452c18c942dff7734aff2f010d89014f0ea15eedaa8/images/34638748_7ER.png" esd-img-prev-position="right top" class="esd-structure es-p10t es-p10b es-p20r es-p20l" style="background-color:#f9eed8;background-image:url(https://tlr.stripocdn.email/content/guids/CABINET_a6898ed919b3f403b400f452c18c942dff7734aff2f010d89014f0ea15eedaa8/images/34638748_7ER.png);background-repeat:no-repeat;background-position:right top">
+                              <!--[if mso]><table dir="ltr" cellpadding="0" cellspacing="0"><tr><td><table dir="rtl" width="560" cellpadding="0" cellspacing="0"><tr><td dir="ltr" width="327" valign="top"><![endif]-->
+                              <table cellpadding="0" cellspacing="0" align="right" class="es-right">
+                                <tbody>
+                                  <tr>
+                                    <td width="327" align="left" class="esd-container-frame es-m-p20b">
+                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                          <tr>
+                                            <td align="left" class="esd-block-text es-p25b">
+                                              <h2 style="color:#003186">
+                                                Hai Kak
+                                                {{$data['name']}},
+                                              </h2>
+                                            </td>
+                                          </tr>
+                                          <tr>
+                                            <td align="left" class="esd-block-text">
+                                              <p style="color:#003186">
+                                                <strong>Selamat hari ini kamu naik level jadi {{$data['levelMembershipNow']}} UD. Djaya!</strong>
+                                              </p>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="327" align="left" class="esd-container-frame">
+                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                          <tr>
+                                            <td align="left" class="esd-block-text es-p35t es-p10b">
+                                              <p style="color:#003186">
+                                                Sebagai reward kamu telah mengumpulkan EXP {{$data['exp']}}, kamu berhak mendapatkan
+                                                @foreach($data['reward'] as $reward)
+                                                 {{$reward->name}},
+                                                @endforeach.
+                                              </p>
+                                            </td>
+                                          </tr>
+                                          <tr>
+                                            <td align="left" class="esd-block-text es-p10t es-p10b">
+                                              <p style="color:#003186">
+                                                Caranya cukup tunjukkan e-mail ini ke kasir! <br>
+                                                Ayo kumpulkan poin dan EXP-mu lebih banyak lagi agar bisa naik level ke {{$data['nextMember']}}.
+                                                Member ini berlaku sampai dengan {{$data['expired']}}.
+
+                                                Terima kasih telah menjadi bagian dari {{$data['levelMembership']}} UD. Djaya.
+                                              </p>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <!--[if mso]></td><td dir="ltr" width="20"></td><td dir="ltr" width="213" valign="top"><![endif]-->
+                              <table cellpadding="0" cellspacing="0" align="left" class="es-left">
+                                <tbody>
+                                  <tr>
+                                    <td width="213" align="left" class="esd-container-frame">
+                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                          <tr>
+                                            <td align="center" class="esd-block-image" style="font-size:0px">
+                                              <a target="_blank" href="https://www.instagram.com/ud.djaya/">
+                                                <img src="https://tlr.stripocdn.email/content/guids/CABINET_a6898ed919b3f403b400f452c18c942dff7734aff2f010d89014f0ea15eedaa8/images/10237823_vRH.png" alt="" width="201" style="display:block">
+                                              </a>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <!--[if mso]></td></tr></table></td></tr></table><![endif]-->
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="left" class="esd-structure">
+                              <table cellpadding="0" cellspacing="0" width="100%">
+                                <tbody>
+                                  <tr>
+                                    <td width="600" align="center" valign="top" class="esd-container-frame">
+                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                          <tr>
+                                            <td align="center" class="esd-block-image" style="font-size:0px">
+                                              <a target="_blank">
+                                                <img src="https://tlr.stripocdn.email/content/guids/CABINET_a6898ed919b3f403b400f452c18c942dff7734aff2f010d89014f0ea15eedaa8/images/slice_4.png" alt="" width="600" class="adapt-img" style="display:block">
+                                              </a>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table cellpadding="0" cellspacing="0" align="center" class="es-content">
+                <tbody></tbody>
+              </table>
+              <table cellpadding="0" cellspacing="0" align="center" class="es-footer">
+                <tbody>
+                  <tr>
+                    <td align="center" class="esd-stripe">
+                      <table width="600" cellspacing="0" cellpadding="0" bgcolor="#d03c3c" align="center" class="es-footer-body" style="background-color:#d03c3c">
+                        <tbody>
+                          <tr>
+                            <td align="left" class="esd-structure es-p25t es-p25b es-p20r es-p20l">
+                              <table cellspacing="0" cellpadding="0" width="100%">
+                                <tbody>
+                                  <tr>
+                                    <td width="560" align="left" class="esd-container-frame">
+                                      <table width="100%" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                          <tr>
+                                            <td align="center" class="esd-block-social es-p10t es-p30b" style="font-size:0">
+                                              <table cellpadding="0" cellspacing="0" class="es-table-not-adapt es-social">
+                                                <tbody>
+                                                  <tr>
+                                                    <td align="center" valign="top" class="es-p20r">
+                                                      <a target="_blank" href="https://www.instagram.com/ud.djaya/">
+                                                        <img src="https://tlr.stripocdn.email/content/assets/img/social-icons/circle-white/instagram-circle-white.png" alt="Ig" title="Instagram" width="32" height="32">
+                                                      </a>
+                                                    </td>
+                                                  </tr>
+                                                </tbody>
+                                              </table>
+                                            </td>
+                                          </tr>
+                                          <tr>
+                                            <td align="center" class="esd-block-text">
+                                              <p>
+                                                Segala informasi yang berkaitan dengan member akan kami informasikan melalui email.
+                                              </p>
+                                              <h2>
+                                                <br>
+                                              </h2>
+                                              <p>
+                                                <b>Salam hangat,</b>
+                                              </p>
+                                              <p>
+                                                <b>UD. Djaya Coffee</b>
+                                              </p>
+                                              <p>
+                                                <b>Instagram : @ud.djaya</b>
+                                              </p>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table cellspacing="0" cellpadding="0" align="center" class="esd-footer-popover es-footer">
+                <tbody></tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
