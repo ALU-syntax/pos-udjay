d90de4a82d843e7e5323e9f060b24b869c71653d
woodiechek <68280149+ALU-syntax@users.noreply.github.com>
2025-09-09 09:33:13 +0700

    feat: add created_by field to customers and associate with authenticated user

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -47,6 +47,7 @@ class CustomerController extends Controller
         $lowestBenchmarkRecords = LevelMembership::where('benchmark', $lowestBenchmarkValue)->first();
 
         $customer->level_memberships_id = $lowestBenchmarkRecords->id;
+        $customer->created_by = auth()->user()->id;
 
         $customer->save();
 
